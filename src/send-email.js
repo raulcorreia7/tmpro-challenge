6602081a87f851d8e685185d9586684058c1abaf
Raúl Correia <raulcorreia7@gmail.com>
2021-10-04 20:44:31 +0200

    stub send-email

--- src/send-email.js
+++ src/send-email.js
@@ -1,45 +1,55 @@
 const AWS = require('aws-sdk')
-const ses = new AWS.SES({ apiVersion: 'latest' })
+const SES = new AWS.SES({ apiVersion: 'latest' })
+const UTILS = require('./lib/utils')
 
-/**
- * Send an e-mail
- *
- * @param {string}       subject  The mail title in UTF-8 encoding
- * @param {string}       body     The mail body in UTF-8 encoding
- * @return {Promise}     The promise, or null if callback is specified
- */
-module.exports = function sendEmail (subject, body) {
-  return new Promise((resolve, reject) => {
-    // this must relate to a verified SES account
-    // eslint-disable-next-line no-undef
-    const from = process.env.FROM_NAME + ' <' + process.env.FROM_EMAIL + '>'
-    const params = {
-      Source: from,
-      // eslint-disable-next-line no-undef
-      Destination: { ToAddresses: [ process.env.TO_EMAIL ] },
-      Message: {
-        Subject: {
-          Data: subject,
-          Charset: 'UTF-8'
-        },
-        Body: {
-          Text: {
-            Data: body,
-            Charset: 'UTF-8'
-          }
-        }
-      }
-    }
-    console.log('e-mail params:', params)
+// /**
+//  * Send an e-mail
+//  *
+//  * @param {string}       subject  The mail title in UTF-8 encoding
+//  * @param {string}       body     The mail body in UTF-8 encoding
+//  * @return {Promise}     The promise, or null if callback is specified
+//  */
+// module.exports = function sendEmail (subject, body) {
+//   return new Promise((resolve, reject) => {
+//     // this must relate to a verified SES account
+//     // eslint-disable-next-line no-undef
+//     const from = process.env.FROM_NAME + ' <' + process.env.FROM_EMAIL + '>'
+//     const params = {
+//       Source: from,
+//       // eslint-disable-next-line no-undef
+//       Destination: { ToAddresses: [ process.env.TO_EMAIL ] },
+//       Message: {
+//         Subject: {
+//           Data: subject,
+//           Charset: 'UTF-8'
+//         },
+//         Body: {
+//           Text: {
+//             Data: body,
+//             Charset: 'UTF-8'
+//           }
+//         }
+//       }
+//     }
+//     console.log('e-mail params:', params)
 
-    ses.sendEmail(params, (err, data) => {
-      if (err) {
-        console.error('Failed to send the email:', err.stack || err)
-        reject(err)
-      } else {
-        console.log('e-mail sent:', data)
-        resolve(data)
-      }
-    })
-  })
+//     ses.sendEmail(params, (err, data) => {
+//       if (err) {
+//         console.error('Failed to send the email:', err.stack || err)
+//         reject(err)
+//       } else {
+//         console.log('e-mail sent:', data)
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
+
+
+
+modules.exports.handler = async (event) => {
+
+  console.log("Email was sent : " + UTILS.stringify(event))
+
+  return Promise.resolve();
 }
